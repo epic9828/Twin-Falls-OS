6fd0cdadd62a73f8ad81b2b2057d7ab82731eaf0
epic9828 <fluffykittenbazooka@gmail.com>
2022-05-26 08:09:05 -0700

    moving eerything

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -3,9 +3,9 @@ kernel for tfOS, it is a monolithic kernel
 will support a gui, and user acces to File sys 
 mabye networking?
 */
-#include "kernel/drivers/fs.h"
+#include "kernel/drivers/fs/fs.h"
 #include "kernel/drivers/sys.h"
-
+#include "kernel/drivers/keyboard/keyboard.h"
 
 //blank entry point
 void entry(){
@@ -14,12 +14,13 @@ void entry(){
 
 
 
+
 //init, all the good stuff
 int kinit()
 {
   while(1){
     print("system kernel");
-
+    print("starting bash, initizing gui");
 
   }
   return 0;
